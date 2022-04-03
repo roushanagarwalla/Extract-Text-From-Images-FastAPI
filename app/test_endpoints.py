06be99752fe5e7c66a1576b387567e2d9bd90ad5
Roushan Agarwalla <roshanagarwal375@gmail.com>
2022-04-03 12:07:15 +0530

    added pre-commit

--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -9,10 +9,9 @@ def test_get_home():
     assert response.status_code == 200
     assert response.headers['content-type'] == "application/json"
     assert response.json() == {"Hello": "World"}
-    
+
 def test_post_home():
     response = client.post("/")
     assert response.status_code == 200
     assert response.headers['content-type'] == "application/json"
     assert response.json() == {"message": "Hello World"}
-    
